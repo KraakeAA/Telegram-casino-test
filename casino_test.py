4e1424bcdeb8d38133b6c85c508f48750369747c
KraakeAA <136975329+KraakeAA@users.noreply.github.com>
2025-06-11 01:58:13 +0100

    Update casino_test.py

--- casino_test.py
+++ casino_test.py
@@ -3,7 +3,7 @@ from telethon.sync import TelegramClient
 api_id = 18581709
 api_hash = 'a92974bc6a79f87017552adfd19987d7'
 phone = '+13468382420'  # Your SMSPool number
-sms_code = '49211'     # Code from SMSPool
+sms_code = '99463'     # Code from SMSPool
 
 client = TelegramClient('anon', api_id, api_hash)
 client.start(phone, code_callback=lambda: sms_code)  # Auto-logon
